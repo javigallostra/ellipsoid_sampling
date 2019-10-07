f0a3c8e5fbd9d537de324184553e92b0da9799eb
javigallostra <jgallostra@gmail.com>
2019-10-07 15:31:38 +0200

    Added vector class

---
+++ vector_base.py
@@ -0,0 +1,9 @@
+class vector:
+    """ Vector class."""
+
+    def __init__(self, **args):
+
+    # queremos: producto por escalar a*b
+    # queremos: producto por vector (dot product) a*b
+    # queremos: producto vectorial (cross product) a.cross(b)
+    # queremos: alguna manera de deci que es un vector unitario
