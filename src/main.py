6680e171b352ce189f1e2c48c33366ced1ad47bd
javigallostra <jgallostra@gmail.com>
2020-02-06 15:38:01 +0100

    Removed unnecessary code from main

--- src/main.py
+++ src/main.py
@@ -4,29 +4,20 @@ from sampling_methods.thomson_sampling import ETS
 from base_classes.vector_base import vector
 from base_classes.point_base import point
 
-from basis_to_rt import RAPIDModuleConverter
-
 # configuration
-SAVE = True
-tool_tcp = vector(1500,-120,280)
-part_size = point(200, 200, 1400)
-d_photo = round((1400/2)/0.6, 0)
+size = point(200, 200, 1400)
+d_extra = round((1400/2)/0.6, 0)
 method = "fibonacci"
 n_points = 70
 ichos_tesselation = 2
-crop_z = 200
-rotation_90 = False
-rotation_neg90 = True
-table_angle = 90
-home_extax_value = 133
 
 # compute
-# takes part_size + d_ph as the sphere radius
-radii = part_size + point(1, 1, 1) * d_photo
+# takes size + d_extra as the ellipsoid radii
+radii = size + point(1, 1, 1) * d_extra
 print("[MAIN] - Sampling ellipsoid...")
 if method == "fibonacci":
     ellipsoid = EFS(radii[0], radii[1], radii[2])
-    ellipsoid.sample(n_points, crop_z)
+    ellipsoid.sample(n_points)
 elif method == "ichosaedron":
     ellipsoid = EIS(radii[0], radii[1], radii[2])
     ellipsoid.sample(ichos_tesselation)
@@ -39,69 +30,4 @@ print("[MAIN] - Sampled %d points." %(len(ellipsoid.basis)))
 
 # plot
 print("[MAIN] - Plotting sampled points...")
-ellipsoid.plot(True, False, False, crop_z)
-
-# adjust positions
-print("[MAIN] - Adjusting positions to robot tool...")
-# crop the points below crop_z
-ellipsoid.crop_z(crop_z)
-# get maximum and minimum base z values
-max_base_z = 0
-min_base_z = radii[2]
-for b in ellipsoid.basis:
-    if b.origin[2] > max_base_z:
-        max_base_z = b.origin[2]
-    elif b.origin[2] < min_base_z:
-        min_base_z = b.origin[2]
-
-# adjust rotation around z based on z value
-for b in ellipsoid.basis:
-    b.rotate(-90 * (b.origin[2] - min_base_z) / (max_base_z - min_base_z), 'z')
-print("[MAIN] - Adjusted %d points." %(len(ellipsoid.basis)))
-
-# generate extra rotation points
-rot_90_points = []
-if rotation_90:
-    print("[MAIN] - Generating positive 90 degree rotations...")
-    for b in ellipsoid.basis:
-        b2 = b.copy()
-        b2.rotate(90)
-        rot_90_points.append(b2)
-    print("[MAIN] - Generated %d positive 90 degree rotations." %(len(rot_90_points)))
-        
-rot_neg90_points = []
-if rotation_neg90:
-    print("[MAIN] - Generating negative 90 degree rotations...")
-    for b in ellipsoid.basis:
-        b2 = b.copy()
-        b2.rotate(-90)
-        rot_neg90_points.append(b2)
-    print("[MAIN] - Generated %d negative 90 degree rotations." %(len(rot_neg90_points)))
-
-# add extra rotation points to ellipsoid
-if len(rot_90_points) > 0:
-    print("[MAIN] - Adding positive 90 degree rotation points to ellipsoid...")
-    for b in rot_90_points:
-        ellipsoid.add_point(b)
-    print("[MAIN] - Added %d positive 90 degree rotations to ellipsoid." %(len(rot_90_points)))
-
-if len(rot_neg90_points) > 0:
-    print("[MAIN] - Adding negative 90 degree rotation points to ellipsoid...")
-    for b in rot_neg90_points:
-        ellipsoid.add_point(b)
-    print("[MAIN] - Added %d negative 90 degree rotations to ellipsoid." %(len(rot_neg90_points)))
-          
-# plot
-print("[MAIN] - Plotting final points...")
-ellipsoid.plot(False, True, False, min_base_z)
-
-# export to RAPID
-if SAVE:
-    print("[MAIN] - Exporting points to RAPID code...")
-    module_converter = RAPIDModuleConverter("PRUEBA", tool_tcp)
-    module_converter.create_RAPID_module(ellipsoid, table_angle, home_extax_value)
-
-print("[MAIN] - Finished.")
-
-# -no olvidar que V-Stars recomienda girar 90 grados al menos una vez
-# -emilio recomienda girar otra vez
+ellipsoid.plot(True, False, False)
