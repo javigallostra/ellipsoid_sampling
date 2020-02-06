3cdd41184f40a91c514f4e6f8898d9c7e9952cf6
javigallostra <jgallostra@gmail.com>
2020-02-06 10:40:55 +0100

    Improved Fibonacci sampling on ellipsoids by sampling directly on the ellipsoidal surface instead of sampling on a sphere and then projecting back to the ellipsoid.

--- fibonacci_sampling.py
+++ fibonacci_sampling.py
@@ -10,8 +10,9 @@ class EFS(ellipsoid_sampling):
 
     Class to sample an ellipsoid with an arbitrary
     number of points. The points are distributed using
-    the Fibonacci Spiral algorithm on a unit sphere
-    and then backprojected to the ellipsoidal surface.
+    the Fibonacci Spiral algorithm directly on the
+    ellipsoidal surface. Alternatively they can be distributed
+    on a unit sphere and then backprojected to the ellipsoidal surface.
     """
 
     def __init__(self, rx=1, ry=1, rz=1):
@@ -42,7 +43,7 @@ class EFS(ellipsoid_sampling):
 
         return lowest_z
 
-    def _spiral(self, n_points, min_z=None):
+    def _spiral_sphere(self, n_points, min_z=None):
         """ Distribute n_points on a Fibonacci Spiral.
 
         The spiral is created on a unit sphere and then
@@ -71,6 +72,47 @@ class EFS(ellipsoid_sampling):
             p = point(math.cos(long)*r, math.sin(long)*r, z)
             p = self._point_ellipsoid_projection(p)
             points.append(p)
+            z -=  dz
+            long += dlong
+        # Return
+        return points
+
+    def _spiral(self, n_points, min_z=None):
+        """ Distribute n_points on a Fibonacci Spiral.
+
+        The spiral is created on the ellipsoidal
+        surface.
+
+        An optional min_z parameter is available to decide
+        the z coordinate of the lowest point.
+
+        Return the list of points.
+        """
+        
+        # If no limit is given, take the full ellipsoid
+        if min_z == None:
+            min_z = -self.rz
+        # Create spiral on ellipsoid
+        dlong = math.pi*(3 - math.sqrt(5))
+        dz = (self.rz - min_z) / n_points
+        long = 0
+        z = self.rz - dz/2
+        points = []
+        for i in range(n_points):
+            # Solve ellipsoid equation
+            num = self.rx**2 * self.ry**2 * (1 - (z**2) / self.rz**2)
+            den = self.ry**2 + self.rx**2 * math.tan(long)**2
+            x = math.sqrt(num / den)
+            # Solve x's sign
+            angle = math.degrees(long) % 360
+            if angle > 90 and angle < 270:
+                x = -x
+            # Compute y
+            y = x * math.tan(long)
+            # Generate point
+            p = point(x, y, z)
+            points.append(p)
+            # Next point
             z -=  dz
             long += dlong
         # Return
